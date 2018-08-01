3b9c1d059bd82464cba203a65acb5cc58f409634
DESKTOP-M8L1VO6\admin <weihanwen1991@gmail.com>
2018-08-01 23:08:14 +0800

    调整

--- project/youdao-bus/src/main/java/com/platform/quartz/orderJob.java
+++ project/youdao-bus/src/main/java/com/platform/quartz/orderJob.java
@@ -36,9 +36,9 @@ public class orderJob {
     /**
      * 订单的定时通知处理
      */
-    @Scheduled(fixedDelay = 60*1000)
+    @Scheduled(fixedDelay = 60 * 1000)
     public void notifyJob() {
-        System.out.println("进入定时器");
+        System.out.println("进入定时器" + DateUtil.getTime());
         long nowTime = System.currentTimeMillis();
         PageData pd = new PageData();
         pd.put("status", "1");
@@ -46,23 +46,28 @@ public class orderJob {
         Map<String, String> map = new HashMap<>();
         map.put("type", "2");
         for (PageData orderpd : list) {
-            long createTime = DateUtil.fomatDate2(orderpd.get("createTime").toString()).getTime();
-            if (nowTime - createTime > 5 * 1000) {
+            String orderUuid = orderpd.getString("orderUuid");
+            String createTimeStr = orderpd.get("createTime").toString();
+            long createTime = DateUtil.fomatDate2(createTimeStr).getTime();
+
+            System.out.println("订单号：" + orderUuid + "**创建时间" + createTime + "》》》》》现在时间" + nowTime);
+
+            if (nowTime - createTime > 5 * 60 * 1000) {
                 //修改状态对超过5分钟未报价的订单系统提示后台管理介入人工处理
-                String message =  "【道巴士】" + "订单【"+orderpd.getString("orderUuid")+"】无人报价中,请前往处理";
-                List<PageData> listSystem =  userMapper.selectList(map);
+                String message = "【道巴士】" + "订单【" + orderUuid + "】无人报价中,请前往处理";
+                List<PageData> listSystem = userMapper.selectList(map);
                 for (PageData sysPd : listSystem) {
                     String phone = sysPd.getString("phone");
                     JSONObject sms = SendSmsUtil.sendSms(message, phone);
                     if (sms.getString("code").equals("0")) {
-                         continue;
+                        continue;
                     }
                 }
-                orderMapper.update("2",orderpd.getString("uuid"),orderpd.getString("orderUuid"));
-            } else if (nowTime - createTime > 4 * 1000) {
+                orderMapper.update("2", orderpd.getString("uuid"), orderUuid);
+            } else if (nowTime - createTime > 4 * 60 * 1000) {
                 //修改状态对超过4分钟未报价的订单平台对车队管理人员手机发短信提示
-                String message =  "【道巴士】" + "订单【"+orderpd.getString("orderUuid")+"】正在报价中,请前往参加";
-                List<PageData> listFleet =  userMapper.selectListBus(map);
+                String message = "【道巴士】" + "订单【" + orderUuid + "】正在报价中,请前往参加";
+                List<PageData> listFleet = userMapper.selectListBus(map);
                 for (PageData fleetPd : listFleet) {
                     String tel = fleetPd.getString("tel");
                     JSONObject sms = SendSmsUtil.sendSms(message, tel);
@@ -72,7 +77,7 @@ public class orderJob {
                         messageMapper.save(pd);
                     }
                 }
-            }else{
+            } else {
                 continue;
             }
         }
